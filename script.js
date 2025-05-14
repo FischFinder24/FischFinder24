f15bb654a2ad52929781286e129c4c5cfac96d5f
FischFinder24 <Fischfinder24@gmail.com>
2025-05-14 14:46:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DEINE Supabase-Zugangsdaten hier einfügen:
-const supabaseUrl = https://xnauxpkrcwpdtvezxxfj.supabase.com
+const supabaseUrl = https://xnauxpkrcwpdtvezxxfj.supabase.co
 const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuYXV4cGtyY3dwZHR2ZXp4eGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMjIyMTcsImV4cCI6MjA2Mjc5ODIxN30.SvjV6zh_rBJ94z9AXbbH5aqt2U-RAkoLzgAmuChKDK4
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
